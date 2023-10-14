88b8daef09725924e6486c47cb37979d31a733d4
Laeti-dev <laetdi@gmail.com>
2023-10-14 14:35:08 +0200

    create custom hook use-http

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-practice-43e51-default-rtdb.europe-west1.firebasedatabase.app'
+        'https://react-practice-43e51-default-rtdb.europe-west1.firebasedatabase.app/Tasks.json'
       );
 
       if (!response.ok) {
